41ac943a46c0524cdf83d2cc4e27614540631e3c
Ralph Ganszky <ralph@ganszky.de>
2020-12-05 17:53:55 +0100

    Add C++ version

---
+++ 5/code_one.cpp
@@ -0,0 +1,52 @@
+//
+// Advent of Code 2020
+//       Day 5
+//
+// © Ralph Ganszky
+//
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+int32_t decode_seat(const std::string& code) {
+    int32_t seat =
+        std::accumulate(code.cbegin(), code.cend(), 0, [&](int32_t a, const char b) -> int32_t {
+            return (a << 1) | ((~b & 0x4) >> 2);
+        });
+    return seat;
+}
+
+int main(int argc, char* argv[]) {
+    std::vector<int32_t> occupied;
+
+    // Determine input filename - default in.txt
+    std::string in_name = "in.txt";
+    if (argc > 1) {
+        if (std::filesystem::exists(argv[1])) {
+            in_name = argv[1];
+        }
+    }
+    std::filesystem::path input_path(in_name);
+    std::ifstream in(input_path);
+    if (!in.is_open())
+        throw std::runtime_error(std::string("Can't open ") + input_path.string() +
+                                 " for reading!");
+
+    // Read input file into occupied
+    std::vector<std::string> in_str;
+    while (!in.eof()) {
+        std::string line;
+        std::getline(in, line);
+        auto seat = decode_seat(line);
+        occupied.push_back(seat);
+    }
+    occupied.pop_back();
+    std::cout << "Highest Seat ID: " << *std::max_element(occupied.begin(), occupied.end())
+              << std::endl;
+
+    return 0;
+}
