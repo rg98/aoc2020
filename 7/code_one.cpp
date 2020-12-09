1ffd7c755b1f0c540d7d2b490d5f650b3764bb43
Ralph Ganszky <ralph@ganszky.de>
2020-12-09 17:51:04 +0100

    Add C++

--- 7/code_one.cpp
+++ 7/code_one.cpp
@@ -1,34 +1,98 @@
 //
 // Advent of Code 2020
-//       Day 6
+//       Day 7
 //
 // © Ralph Ganszky
 //
 
-#include <cctype>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <numeric>
+#include <regex>
 #include <string>
+#include <tuple>
 #include <vector>
 
-// Check password
+// Color
 //
 
-int count_questions(const std::string& group) {
-    std::map<char, int> questions;
-    for (auto c : group)
-        if (std::islower(c)) {
-            if (questions.find(c) != questions.end())
-                questions[c]++;
-            else
-                questions[c] = 1;
-        } else {
-            throw std::runtime_error("wrong format in count_question!");
+class Color {
+  public:
+    Color(const std::string& name) : _name(name) {}
+    const std::string& name() const { return _name; }
+
+  private:
+    std::string _name;
+};
+
+bool operator==(Color lhr, const Color rhr) { return lhr.name().compare(rhr.name()) == 0; }
+bool operator<(Color lhr, const Color rhr) { return lhr.name().compare(rhr.name()) < 0; }
+
+std::ostream& operator<<(std::ostream& os, const Color& color) {
+    os << color.name();
+    return os;
+}
+
+// Edge
+//
+
+class Edge {
+  public:
+    Edge(const std::size_t from, const ssize_t bags, const std::size_t to)
+        : _from(from), _to(to), _bags(bags) {}
+    const auto from() const { return _from; }
+    const auto to() const { return _to; }
+    const auto bags() const { return _bags; }
+
+  private:
+    std::size_t _from;
+    std::size_t _to;
+    ssize_t _bags;
+};
+
+std::ostream& operator<<(std::ostream& os, const Edge& edge) {
+    os << edge.from() << " contains " << edge.bags() << " of " << edge.to();
+    return os;
+}
+
+// get_bags
+//
+
+std::vector<std::size_t> get_bags(std::size_t to_color,
+                                  std::map<std::size_t, std::vector<Edge>>& edge_by_color) {
+    std::vector<std::size_t> bags;
+    for (auto& i : edge_by_color[to_color]) {
+        if (i.bags() < 0) {
+            if (std::find(bags.begin(), bags.end(), i.to()) == bags.end()) {
+                bags.push_back(i.to());
+                auto _bags = get_bags(i.to(), edge_by_color);
+                bags.insert(bags.end(), _bags.begin(), _bags.end());
+            }
         }
-    return questions.size();
+    }
+    return bags;
+}
+
+inline std::size_t insert_when_not_found(const std::string& color_name,
+                                         std::vector<Color>& nodes) {
+    Color color(color_name);
+    auto c_it = std::find(nodes.begin(), nodes.end(), color);
+    if (c_it == nodes.end()) {
+        nodes.push_back(color);
+        return std::distance(nodes.begin(), std::prev(nodes.end()));
+    }
+    return std::distance(nodes.begin(), c_it);
+}
+
+inline std::size_t get_color_id(const Color& color, const std::vector<Color>& colors) {
+    auto color_it = std::find(colors.begin(), colors.end(), color);
+    if (color_it == colors.end())
+        throw std::runtime_error(std::string("Can't find color ") + color.name() +
+                                 " in colors!");
+    return std::distance(colors.begin(), color_it);
 }
 
 int main(int argc, char* argv[]) {
@@ -45,22 +109,79 @@ int main(int argc, char* argv[]) {
         throw std::runtime_error(std::string("Can't open ") + input_path.string() +
                                  " for reading!");
 
-    // Read input file into maze
-    std::vector<std::string> groups = {""};
+    // Regular expression
+    std::regex sep(" bags contain | bags?(,|\\.) ?");
+    std::regex re_edge("(\\d+) (\\w+ \\w+)");
+
+    // Nodes and edges
+    std::vector<Color> nodes;
+    std::vector<Edge> edges;
+
+    // Read input file into nodes and edges
     std::string line;
     while (std::getline(in, line)) {
-        if (line.size() > 0)
-            groups.back() += line;
-        else
-            groups.push_back("");
+        auto it = std::sregex_token_iterator(line.begin(), line.end(), sep, -1);
+        auto from_color = insert_when_not_found(*it, nodes);
+        for (it++; it != std::sregex_token_iterator(); it++) {
+            std::string s_edge(*it);
+            auto e_it =
+                std::sregex_token_iterator(s_edge.begin(), s_edge.end(), re_edge, {1, 2});
+            if (e_it != std::sregex_token_iterator()) {
+                auto n = std::stoi(*e_it++);
+                auto to_color = insert_when_not_found(*e_it, nodes);
+                edges.emplace_back(Edge(from_color, n, to_color));
+            } else {
+                if (s_edge.compare("no other") != 0)
+                    throw std::runtime_error(std::string("Can't analyze edge - ") + s_edge +
+                                             "!");
+            }
+        }
+    }
+    std::cout << "Found " << nodes.size() << " nodes with " << edges.size() << " edges.\n";
+
+    // std::cout << "Colors:\n";
+    // for (auto i{0}; i < nodes.size(); i++)
+    //     std::cout << "\t" << i << ": " << nodes[i].name() << '\n';
+
+    // Build graph in edge_by_color
+    std::map<std::size_t, std::vector<Edge>> graph_edge_by_color;
+    for (auto& edge : edges) {
+        // std::cout << "mapping: " << nodes[edge.from()] << " -> " << edge.bags() << " of "
+        //           << nodes[edge.to()] << '\n';
+        if (graph_edge_by_color.find(edge.from()) == graph_edge_by_color.end()) {
+            graph_edge_by_color[edge.from()] = std::vector<Edge>(1, edge);
+        } else {
+            graph_edge_by_color[edge.from()].push_back(edge);
+        }
+        if (graph_edge_by_color.find(edge.to()) == graph_edge_by_color.end()) {
+            graph_edge_by_color[edge.to()] =
+                std::vector<Edge>(1, Edge(edge.to(), -edge.bags(), edge.from()));
+        } else {
+            graph_edge_by_color[edge.to()].emplace_back(
+                Edge(edge.to(), -edge.bags(), edge.from()));
+        }
     }
 
-    auto count{0};
-    for (auto& group : groups) {
-        count += count_questions(group);
-    }
+    // std::cout << "Graph:\n";
+    // for (auto c : graph_edge_by_color) {
+    //     std::cout << "Color " << c.first << " " << nodes[c.first] << ":\n";
+    //     for (auto e : c.second) {
+    //         if (e.bags() > 0)
+    //             std::cout << "\tEdge: " << nodes[e.from()] << " -> " << e.bags() << " of "
+    //                       << nodes[e.to()] << '\n';
+    //         else
+    //             std::cout << "\tEdge: " << nodes[e.from()] << " <- " << e.bags() << " of "
+    //                       << nodes[e.to()] << '\n';
+    //     }
+    // }
 
-    std::cout << "Sum of all groups: " << count << std::endl;
+    auto bags = get_bags(get_color_id(Color("shiny gold"), nodes), graph_edge_by_color);
+
+    // Remove duplicate colors
+    std::sort(bags.begin(), bags.end());
+    bags.erase(std::unique(bags.begin(), bags.end()), bags.end());
+
+    std::cout << bags.size() << " can eventually contain at least one shiny gold bag!\n";
 
     return 0;
 }
